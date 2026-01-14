0e0a3e36ae9f64daf9b1a14c780c62351f679560
tbunitrade <tbunitrade@gmail.com>
2026-01-14 19:02:08 +0200

    Fix instalation issues for trio

--- tests/conftest.py
+++ tests/conftest.py
@@ -107,3 +107,7 @@ async def async_client():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         yield ac
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
